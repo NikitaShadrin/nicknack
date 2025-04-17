75760872881ca649a37c45cfd2e1b2c81d29b9bf
Nikita Shadrin <shadrinnikita@gmail.com>
2025-04-17 04:03:56 +0300

    added a third project

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -1,10 +1,10 @@
 type Project = {
-    id: number;
-    name: string;
-    image: string;
-    repo: string;
-    demo: string;
-    description: string;
+  id: number;
+  name: string;
+  image: string;
+  repo: string;
+  demo: string;
+  description: string;
 };
 
 export const projects: Project[] = [
@@ -14,7 +14,8 @@ export const projects: Project[] = [
     image: "/images/lando_preview.png",
     repo: "https://github.com/NikitaShadrin/lando-project",
     demo: "",
-    description: "A mock landing page for a SAAS product to practice some React and Tailwind.",
+    description:
+      "A mock landing page for a SAAS product to practice some React and Tailwind.",
   },
   {
     id: 2,
@@ -22,6 +23,16 @@ export const projects: Project[] = [
     image: "/images/gitrepofind_preview.png",
     repo: "https://github.com/NikitaShadrin/github_repo_pinner",
     demo: "https://nikitashadrin.github.io/github_repo_pinner",
-    description: "A page that lets you fetch and pin different Github repos. Decent practice for rest API mostly",
+    description:
+      "A page that lets you fetch and pin different Github repos. Decent practice for rest API mostly",
+  },
+  {
+    id: 3,
+    name: "Personal Blog",
+    image: "/images/nick_blog_preview.png",
+    repo: "https://github.com/NikitaShadrin/nick-blog",
+    demo: "https://nick-blog-topaz.vercel.app",
+    description:
+      "A personal blog where I write about my projects and other things I'm learning in the SWE space.",
   },
 ];
